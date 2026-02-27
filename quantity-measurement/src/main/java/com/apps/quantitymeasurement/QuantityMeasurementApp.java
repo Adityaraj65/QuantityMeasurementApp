bf2c5c30ca0687317edb51327baafc333d655c5a
Aditya <ar850732@gmail.com>
2026-02-27 13:12:24 +0530

    [Aditya Raj] refactor. modify.Unit-to-Unit Conversion   in feature/UC5-Unit-to-Unit Conversion

--- quantity-measurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ quantity-measurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,29 +2,39 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    // generic equality check for any length units
+    // UC3 / UC4 equality demo
     public static boolean demonstrateLengthEquality(Length l1, Length l2) {
         return l1.equals(l2);
     }
 
-    // helper method to compare two values with units
+    // UC3 style comparison using raw values
     public static boolean demonstrateLengthComparison(
-            double value1, Length.LengthUnit unit1,
-            double value2, Length.LengthUnit unit2) {
+            double v1, Length.LengthUnit u1,
+            double v2, Length.LengthUnit u2) {
 
-        Length length1 = new Length(value1, unit1);
-        Length length2 = new Length(value2, unit2);
-
-        return length1.equals(length2);
+        return new Length(v1, u1).equals(new Length(v2, u2));
     }
 
-    // manual verification
+    // UC5: static conversion API (raw values)
+    public static Length demonstrateLengthConversion(
+            double value,
+            Length.LengthUnit fromUnit,
+            Length.LengthUnit toUnit) {
+
+        return new Length(value, fromUnit).convertTo(toUnit);
+    }
+
+    // UC5: overloaded conversion API (object based)
+    public static Length demonstrateLengthConversion(
+            Length length,
+            Length.LengthUnit toUnit) {
+
+        return length.convertTo(toUnit);
+    }
+
     public static void main(String[] args) {
-
-        System.out.println(demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
-                36.0, Length.LengthUnit.INCHES));
-
-        System.out.println(demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS,
-                39.3701, Length.LengthUnit.INCHES));
+        System.out.println(demonstrateLengthConversion(3.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES));
+        System.out.println(demonstrateLengthConversion(2.0, Length.LengthUnit.YARDS, Length.LengthUnit.INCHES));
+        System.out.println(demonstrateLengthConversion(1.0, Length.LengthUnit.CENTIMETERS, Length.LengthUnit.INCHES));
     }
 }
