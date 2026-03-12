c2376d9d23d6e6854b6ed27fac8ae6ef933ae60c
Aditya <ar850732@gmail.com>
2026-03-12 23:37:19 +0530

    [Aditya Raj]Add.Added feature/UC16-Database-Integration-with-JDBC-for-Quantity-Measurement-Persistence

---
+++ quantity-measurement/src/main/java/com/apps/quantitymeasurement/exception/DatabaseException.java
@@ -0,0 +1,5 @@
+package com.apps.quantitymeasurement.exception;
+
+public class DatabaseException {
+
+}
---
+++ quantity-measurement/src/main/java/com/apps/quantitymeasurement/unit/IMeasurable.java
@@ -0,0 +1,5 @@
+package com.apps.quantitymeasurement.unit;
+
+public class IMeasurable {
+
+}
